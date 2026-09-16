db4a1148fc71ed76ce3a7399743ff68e0040d1b7
Khadi <khadidiamahamanet@gmail.com>
2026-09-16 22:46:39 +0000

    Je suis montée sur REACT

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <h1 className="bg-blue-500">BONJOUR</h1>
+      </section>
+
+     
+    </>
+  )
+}
+
+export default App
